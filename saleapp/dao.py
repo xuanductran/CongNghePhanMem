05f3545a378c4a7eaf3d6a740c436a0a6b64bd39
xuanductran <tranxuanduc.work@gmail.com>
2024-11-01 16:37:50 +0700

    edit

--- saleapp/dao.py
+++ saleapp/dao.py
@@ -1,7 +1,7 @@
 import json
 
 
-def load_categories(c_id=None):
+def load_categories():
     with open('data/categories.json', encoding='utf-8') as f:
         categories = json.load(f)
 
@@ -17,6 +17,12 @@ def load_products(q=None,c_id=None):
 
         return products
 
+def load_product_by_id(id):
+    with open('data/products.json', encoding='utf-8') as f:
+        products = json.load(f)
+        for p in products:
+            if(p["id"]==id):
+                return p
 
 if __name__=="__main__":
     print(load_products())
--- saleapp/index.py
+++ saleapp/index.py
@@ -1,23 +1,27 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 import dao
-
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    q=request.args.get("q")
-    c_id=request.args.get("category")
-    categories = dao.load_categories(c_id)
-    products = dao.load_products(q,c_id)
+    q = request.args.get("q")
+    c_id = request.args.get("category")
+
+    products = dao.load_products(q, c_id)
+    categories = common_attributes()["categories"]  # Gọi hàm để lấy categories
     return render_template('index.html', categories=categories, products=products)
 
 @app.route('/product/<int:id>')
 def get_product(id):
+    product = dao.load_product_by_id(id)
+    return render_template('product.html', product=product)
 
-    return render_template('product.html')
-
+@app.context_processor
+def common_attributes():
+    return {
+        "categories": dao.load_categories()
+    }
 
 if __name__ == "__main__":
-    with app.app_context():
-        app.run(debug=True)
+    app.run(debug=True)
